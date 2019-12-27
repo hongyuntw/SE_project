d10c735a656660dcb59ec90aad84d1ce7c42349f
HongYun0901 <harrychiang0@gmail.com>
2019-12-27 16:19:43 +0800

    remove empty

--- se.py
+++ se.py
@@ -28,15 +28,12 @@ delimiters_dict = {
     ':': 'COLON',
 }
 keyword_dict = {
-    'break': 'BREAK',
     'case': 'CASE',
-    'continue': 'CONTINUE',
     'do': 'DO',
     'else': 'ELSE',
     'for': 'FOR',
     'goto': 'GOTO',
     'if': 'IF',
-    'return': 'RETURN',
     'switch': 'SWITCH',
     'while': 'WHILE',
     'default':'DEFAULT'
@@ -54,15 +51,12 @@ def t_CONTENT(t):
 
 
 # keyword
-t_BREAK = r'break'
 t_CASE = r'case'
-t_CONTINUE = r'continue'
 t_DO = r'do'
 t_ELSE = r'else'
 t_FOR = r'for'
 t_GOTO = r'goto'
 t_IF = r'if'
-t_RETURN = r'return'
 t_SWITCH = r'switch'
 t_WHILE = r'while'
 t_DEFAULT = r'default'
@@ -108,14 +102,20 @@ switch(c){
     case 1:
     case 2:
         thisistwo;
+        break;
     case 3:
         3333;
+        break;
 }
 a--;
 if(c){
     b--;
 }
-
+int c=0;
+for(int i=0;i<3;i++){
+    c--;
+}
+a--;
 '''
 
 casedict = {}
@@ -123,6 +123,8 @@ myowndict = {}
 lexer.input(data)
 need_add_edge = {}
 defaultnodes = {}
+alledge =  []
+allnode = {}
 # Tokenize
 while True:
     tok = lexer.token()
@@ -145,6 +147,7 @@ def GetInitData():
 def p_stmts(p):
     '''stmts : stmt
              | stmt stmts'''
+    global alledge
     p[0] = GetInitData()
     if(len(p) == 2):
         p[0]['headNodes'] = p[1]['headNodes']
@@ -155,6 +158,7 @@ def p_stmts(p):
         for startNode in p[1]['tailNodes']:
             for endNode in p[2]['headNodes']:
                 g.edge(startNode, endNode)
+                alledge.append((startNode,endNode,''))
 
 
 def p_stmt(p):
@@ -171,6 +175,7 @@ def p_stmt(p):
         p[0]['headNodes'] = [f'{seq}.{layer}.{empty}']
         p[0]['tailNodes'] = p[0]['headNodes']
         g.node(f'{seq}.{layer}.{empty}', 'empty')
+        allnode[f'{seq}.{layer}.{empty}']= 'empty'
         myowndict[f'{seq}.{layer}.{empty}'] = 'empty'
         empty += 1
     else:
@@ -183,12 +188,14 @@ def p_switch_stmt(p):
     '''switch_stmt : SWITCH LP bool_expr RP LBRACE case_stmt RBRACE stmt'''
     global myowndict
     global casedict
+    global alledge
     flag = False
     p[0] = GetInitData()
     # p0 headnode?
     edgedict = {}
     for head in p[6]['headNodes']:
         g.edge(p[3]['tailNodes'][0], head, label='switch')
+        alledge.append((p[3]['tailNodes'][0],head,'switch'))
         if (head in casedict):
             pass
         else:
@@ -197,9 +204,11 @@ def p_switch_stmt(p):
             belinknode = f'{seq_t}.{layer_t}'
             if(head in defaultnodes):
                 g.edge(head, defaultnodes[head][-1])
+                alledge.append((head,defaultnodes[head][-1],''))
                 flag = True
             else:
                 g.edge(head, belinknode)
+                alledge.append((head,belinknode,''))
                 edgedict[head] = belinknode
     newcasedict = {}
     i = 0
@@ -227,14 +236,17 @@ def p_switch_stmt(p):
 
     for k,v in newcasedict.items():
         g.edge(k, newcasedict[k])
+        alledge.append((k,newcasedict[k],''))
         
     for tail in p[6]['tailNodes']:
         g.edge(tail,p[8]['tailNodes'][0])
+        alledge.append((tail,p[8]['tailNodes'][0],''))
     p[0]['headNodes'].append(p[3]['headNodes'][0])
     p[0]['tailNodes'] = p[8]['tailNodes']
 #  default
     if (not flag):
-        g.edge(p[3]['headNodes'][0],p[8]['headNodes'][0],label='nodefault')
+        g.edge(p[3]['headNodes'][0],p[8]['headNodes'][0])
+        alledge.append((p[3]['headNodes'][0],p[8]['headNodes'][0],''))
 
     
         
@@ -250,12 +262,15 @@ def p_case_stmt(p):
     global casedict
     global defaultnodes
     global layer
+    global alledge
+    global allnode
     if len(p) == 5:
         p[0]['headNodes'] = p[2]['headNodes']
         p[0]['tailNodes'] = p[4]['tailNodes']
     elif len(p) == 4:
         print('default')
         g.node(f'{seq}.{layer}', label='default', shape='box')
+        allnode[f'{seq}.{layer}'] = 'default'
         p[0]['headNodes'].append(f'{seq}.{layer}')
         p[0]['tailNodes'] = p[3]['tailNodes']
         defaultnodes[f'{seq}.{layer}'] = p[3]['tailNodes']
@@ -282,18 +297,25 @@ def p_case_stmt(p):
 def p_ifstmt(p):
     '''ifstmt : IF LP bool_expr RP LBRACE stmts RBRACE elif stmt
               | IF LP bool_expr RP LBRACE stmts RBRACE stmt'''
+    global alledge
     p[0] = GetInitData()
     p[0]['headNodes'] = p[3]['headNodes']
     g.edge(p[3]['tailNodes'][0], p[6]['headNodes'][0], label='true')
+    alledge.append((p[3]['tailNodes'][0],p[6]['headNodes'][0],'true'))
+    
     if(len(p) == 10):
         p[0]['tailNodes'] = p[9]['tailNodes']
         g.edge(p[3]['tailNodes'][0], p[8]['headNodes'][0], label='false')
+        alledge.append((p[3]['tailNodes'][0],p[8]['headNodes'][0],'false'))
         for tail in (p[6]['tailNodes'] + p[8]['tailNodes']):
             g.edge(tail, p[9]['headNodes'][0])
+            alledge.append((tail,p[9]['headNodes'][0],''))
     elif(len(p) == 9):
         p[0]['tailNodes'] = p[8]['tailNodes']
         g.edge(p[3]['tailNodes'][0], p[8]['headNodes'][0], label='false')
+        alledge.append((p[3]['tailNodes'][0],p[8]['headNodes'][0],'false'))
         g.edge(p[6]['tailNodes'][0], p[8]['headNodes'][0])
+        alledge.append((p[6]['tailNodes'][0],p[8]['headNodes'][0],''))
 
 
 def p_elseif_s(p):
@@ -301,19 +323,23 @@ def p_elseif_s(p):
              | elif
              | elif elifs '''
     p[0] = GetInitData()
+    global alledge
     if(len(p) == 2):
         p[0] = p[1]
     elif(len(p) == 3):
         p[0]['headNodes'] = p[1]['headNodes']
         p[0]['tailNodes'] = p[1]['tailNodes'] + p[2]['tailNodes']
         g.edge(p[1]['headNodes'][0], p[2]['headNodes'][0], label='false')
+        alledge.append((p[1]['headNodes'][0],p[2]['headNodes'][0],'false'))
 
 
 def p_elseif(p):
     '''elif : ELSE IF LP bool_expr RP LBRACE stmts RBRACE'''
+    global alledge
     p[0] = {'headNodes': p[4]['headNodes'],
             'tailNodes': p[7]['tailNodes']}
     g.edge(p[4]['tailNodes'][0], p[7]['headNodes'][0], label='true')
+    alledge.append((p[4]['tailNodes'][0],p[7]['headNodes'][0],'true'))
 
 
 def p_else(p):
@@ -325,35 +351,49 @@ def p_else(p):
 def p_whilestmt(p):
     '''whilestmt : WHILE LP bool_expr RP LBRACE stmts RBRACE stmt'''
     p[0] = GetInitData()
+    global alledge
     p[0]['headNodes'] = p[3]['headNodes']
     p[0]['tailNodes'] = p[8]['tailNodes']
     # true
     g.edge(p[3]['tailNodes'][0], p[6]['headNodes'][0], label='true')
+    alledge.append((p[3]['tailNodes'][0],p[6]['headNodes'][0],'true'))
     # false
     g.edge(p[3]['tailNodes'][0], p[8]['headNodes'][0], label='false')
+    alledge.append((p[3]['tailNodes'][0],p[8]['headNodes'][0],'false'))
     # loop
     g.edge(p[6]['tailNodes'][0], p[3]['headNodes'][0], label='loop')
+    alledge.append((p[6]['tailNodes'][0],p[3]['headNodes'][0],'loop'))
 
 
 def p_forstmt(p):
     '''forstmt : FOR LP for_expr RP LBRACE stmts RBRACE stmt'''
+    global alledge
     p[0] = GetInitData()
     p[0]['headNodes'] = p[3]['initNodes']
     p[0]['tailNodes'] = p[8]['tailNodes']
     g.edge(p[3]['initNodes'][0], p[3]['boolNodes'][0])
+    alledge.append((p[3]['initNodes'][0],p[3]['boolNodes'][0],''))
     g.edge(p[3]['boolNodes'][0], p[6]['headNodes'][0], label='true')
+    alledge.append((p[3]['boolNodes'][0],p[6]['headNodes'][0],'true'))
     g.edge(p[3]['boolNodes'][0], p[8]['headNodes'][0], label='false')
+    alledge.append((p[3]['boolNodes'][0],p[8]['headNodes'][0],'false'))
     g.edge(p[6]['tailNodes'][0], p[3]['postNodes'][0], label='For Routine')
+    alledge.append((p[6]['tailNodes'][0],p[3]['postNodes'][0],'For Routine'))
     g.edge(p[3]['postNodes'][0], p[3]['boolNodes'][0], label='loop')
+    alledge.append((p[3]['postNodes'][0],p[3]['boolNodes'][0],'loop'))
 
 
 def p_for_expr(p):
     '''for_expr : contents SEMI contents SEMI contents'''
     global layer
+    global allnode
     p[0] = GetInitData()
     g.node(f'{seq}.{layer}', p[1], shape='box')
+    allnode[f'{seq}.{layer}'] = p[1]
     g.node(f'{seq}.{layer+1}', p[3], shape='diamond')
+    allnode[f'{seq}.{layer+1}'] = p[3]
     g.node(f'{seq}.{layer+2}', p[5], shape='box')
+    allnode[f'{seq}.{layer+2}'] = p[5]
     p[0]['initNodes'] = [f'{seq}.{layer}']
     p[0]['boolNodes'] = [f'{seq}.{layer+1}']
     p[0]['postNodes'] = [f'{seq}.{layer+2}']
@@ -364,10 +404,12 @@ def p_bool_expr(p):
     '''bool_expr : contents'''
     global layer
     global myowndict
+    global allnode
     p[0] = {'headNodes': [f'{seq}.{layer}'],
             'tailNodes': [f'{seq}.{layer}']}
     # create Node
     g.node(f'{seq}.{layer}', p[1], shape='diamond')
+    allnode[f'{seq}.{layer}'] = p[1]
     myowndict[f'{seq}.{layer}'] = p[1]
 
     layer += 1
@@ -381,6 +423,7 @@ def p_expr(p):
     p[0] = {'headNodes': [f'{seq}.{layer}'],
             'tailNodes': [f'{seq}.{layer}']}
     g.node(f'{seq}.{layer}', p[1] + p[2], shape='box')
+    allnode[f'{seq}.{layer}'] = p[1]+p[2]
     myowndict[f'{seq}.{layer}'] = p[1] + p[2]
 
     layer += 1
@@ -406,4 +449,41 @@ def p_error(p):
 parser = yacc.yacc()
 
 parser.parse(data, lexer=lexer)
+
+# create a new graph without empty
+newg = Digraph('G', filename='newcluster.gv')
+emptylist = []
+for k,v in allnode.items():
+    if v != 'empty':
+        newg.node(k,v)
+    else:
+        emptylist.append(k)
+
+# 處理empty node
+for emptynode in emptylist:
+    for tp in alledge:
+        startnode = tp[0]
+        endnode  = tp[1]
+        label = tp[2]
+        # 有連到empty的
+        if endnode == emptynode:
+            # a->empty->b
+            # 把a->b
+            for tp2 in alledge:
+                startnode2 = tp[0]
+                endnode2  = tp[1]
+                label2 = tp[2]
+                if startnode2 == emptynode:
+                    newg.edge(startNode,endnode2,label=label)
+                        
+for tp in alledge:
+    startnode = tp[0]
+    endnode  = tp[1]
+    label = tp[2] 
+    if not (startnode in emptylist or endnode in emptylist):
+        newg.edge(startnode,endnode,label=label)
+
+
+
 g.view()
+newg.view()
